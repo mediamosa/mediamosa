445c00cecb884ad07630ab1b93b9b7fdf8967ecc
Robert Blanker <rblanker@xs4all.nl>
2013-11-30 15:25:19 +0100

    By rblanker: Updated Drupal to 7.24.

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -551,6 +551,7 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
 /**
  * Default setting, TRUE / FALSE for enabling / disabling the
  * APP REST interface. Putting it on FALSE will disable the interface
